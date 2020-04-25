0c46bc29cb227a99e80053e88bafbfa72a604188
sambujanni <siva.janni11@gmail.com>
2020-04-25 13:18:15 +0530

    fixed fix53

--- src/main/java/com/siva/janni/mysimpleapplication/controller/HelloWorld.java
+++ src/main/java/com/siva/janni/mysimpleapplication/controller/HelloWorld.java
@@ -2,14 +2,15 @@ package com.siva.janni.mysimpleapplication.controller;
 
 import com.siva.janni.mysimpleapplication.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class HelloWorld {
- Set<Student> students = Set.of(new Student(10, "Siv", "B"),
+ List<Student> students = List.of(new Student(10, "Siv", "B"),
          new Student(11, "Raj", "A"),
          new Student(12, "Gopal", "B"),
          new Student(10, "Siv", "B")
@@ -20,8 +21,13 @@ public class HelloWorld {
     }
 
     @GetMapping("/students")
-    public Set<Student> getStudents() {
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Student> getStudent(@PathVariable int id) {
+        return students.stream().filter(s -> s.getId() == id).findFirst();
     }
 
 }
