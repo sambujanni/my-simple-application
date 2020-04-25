c44c99b482ac7e9109cbbf15b63a61562afea006
sambujanni <siva.janni11@gmail.com>
2020-04-25 09:27:47 +0530

    intial checkin

---
+++ src/main/java/com/siva/janni/mysimpleapplication/MySimpleApplication.java
@@ -0,0 +1,13 @@
+package com.siva.janni.mysimpleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySimpleApplication.class, args);
+	}
+
+}
