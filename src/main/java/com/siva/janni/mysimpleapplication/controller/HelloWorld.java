9d3756c622816177985edf3b10fb4a4d933e17ee
sambujanni <siva.janni11@gmail.com>
2020-04-25 09:58:34 +0530

    updated controller

--- src/main/java/com/siva/janni/mysimpleapplication/controller/HelloWorld.java
+++ src/main/java/com/siva/janni/mysimpleapplication/controller/HelloWorld.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
-    @GetMapping
+    @GetMapping("/wish")
     public String wish() {
         return "Hello World!!";
+    }
+
+    @GetMapping("/message")
+    public String message() {
+        return "Boss is back";
     }
 }
