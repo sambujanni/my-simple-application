7c0c6f9582c422f730b0d2a02d07d7c959a6ba96
sambujanni <siva.janni11@gmail.com>
2020-04-25 11:59:53 +0530

    updated controller

--- src/main/java/com/siva/janni/mysimpleapplication/controller/HelloWorld.java
+++ src/main/java/com/siva/janni/mysimpleapplication/controller/HelloWorld.java
@@ -1,13 +1,27 @@
 package com.siva.janni.mysimpleapplication.controller;
 
+import com.siva.janni.mysimpleapplication.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @RestController
 public class HelloWorld {
-
+ Set<Student> students = Set.of(new Student(10, "Siv", "B"),
+         new Student(11, "Raj", "A"),
+         new Student(12, "Gopal", "B"),
+         new Student(10, "Siv", "B")
+         );
     @GetMapping
     public String wish() {
         return "Hello World!!";
     }
+
+    @GetMapping("/students")
+    public Set<Student> getStudents() {
+        return students;
+    }
+
 }
---
+++ src/main/java/com/siva/janni/mysimpleapplication/model/Student.java
@@ -0,0 +1,34 @@
+package com.siva.janni.mysimpleapplication.model;
+
+public class Student {
+    private int id;
+    private String name;
+    private String grade;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public Student(int id, String name, String grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
+}
